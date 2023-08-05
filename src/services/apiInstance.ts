e6443553592053d6fa40524a4ef451dbd1a7d284
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-08-05 17:33:35 +0545

    Implement access key for unsplash within the file

--- src/services/apiInstance.ts
+++ src/services/apiInstance.ts
@@ -7,9 +7,14 @@ export type FetchedDataType = {
   results: FetchedImageType[];
 };
 
+//from .env file, access key can be imported using import.meta.env.VITE_ACCESS_KEY
+//for now, implmenting the access key by providing directly within the file
+
+const ACCESS_KEY = 'DIXBzQZ6CNbwVFFaZVQgC-6FxHePOa-BZ8yBJM50g-U';
+
 export const axiosInstance = axios.create({
   baseURL: 'https://api.unsplash.com/search/photos',
   headers: {
-    Authorization: `Client-ID ${import.meta.env.VITE_ACCESS_KEY}`,
+    Authorization: `Client-ID ${ACCESS_KEY}`,
   },
 });
