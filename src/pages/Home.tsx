529b1b5fef9ffc9dde2f70ddd94f29a4dfdde4d2
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-08-06 13:04:41 +0545

    fixed some issues while deploying

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,7 @@
-import React, { useRef } from 'react';
 import useSearchQuery from '../hooks/useSearchQuery';
-// import useFetchImages, { FetchedImageType } from '../hooks/useFetchImages';
-
-// import InfiniteScroll from 'react-infinite-scroll-component';
-// import ImageGrid from '../components/ImageGrid';
-// import ImageCard from '../components/ImageCard';
-
-// import NavBar from '../components/NavBar';
-// import Message from '../components/Message';
-// import Loader from '../components/Loader';
 import FetchedImages from '../components/FetchedImages';
 import NavBar from '../components/NavBar';
+import { useRef } from 'react';
 
 const Home = () => {
   //useRef hook for getting the input text without rerendering the application
