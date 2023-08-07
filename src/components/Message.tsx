2754f0f482a2cb9e8f2308b0a0666a619dfd77cf
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-08-07 17:54:10 +0545

    Implement styling changes

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -5,7 +5,7 @@ type Props = {
 // This component is used for providing different messages to UI
 const Message = ({ message }: Props) => {
   return (
-    <h1 className='my-10 text-xl font-semibold text-center sm:text-2xl'>
+    <h1 className='p-4 my-10 text-xl font-semibold text-center transition-all sm:text-2xl'>
       {message}
     </h1>
   );
