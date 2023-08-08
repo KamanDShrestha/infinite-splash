3cfdba7135a492ecaaefbd686d3ec064f5e6b9c1
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2023-08-08 11:26:21 +0545

    Implement grid styling changes

--- src/components/ImageGrid.tsx
+++ src/components/ImageGrid.tsx
@@ -7,7 +7,7 @@ type Props = {
 //THis component is used for providing a grid layout to fetched images
 const ImageGrid = ({ children }: Props) => {
   return (
-    <div className='grid grid-cols-1 gap-6 p-6 my-4 sm:grid-cols-2 md:grid-cols-3'>
+    <div className='grid grid-cols-1 gap-6 p-6 my-4 max-[640px]:grid-cols-1 max-[900px]:grid-cols-2 min-[900px]:grid-cols-3'>
       {children}
     </div>
   );
