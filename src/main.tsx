8a483c688776d0d3a2a01a92b15365e1c4608228
KamanDShrestha <109749519+KamanDShrestha@users.noreply.github.com>
2024-08-14 23:23:11 +0545

    add: added analytics component

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App.tsx';
 import './index.css';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Analytics } from '@vercel/analytics/react';
 
 //making a client object to provide to the overall application
 const queryClient = new QueryClient();
@@ -12,6 +13,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
+      <Analytics />
       <ReactQueryDevtools />
     </QueryClientProvider>
   </React.StrictMode>
